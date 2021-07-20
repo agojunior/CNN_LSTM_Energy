7cff96b5369ba1706e1843e2ff3c430208bd343f
Junior <antonio.gomes.o.jr@gmail.com>
2021-07-20 03:36:48 -0300

    initial commit v1 tcc2

---
+++ tcc2 MSE.py
@@ -0,0 +1,408 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+
+gpu_devices = tf.config.experimental.list_physical_devices("GPU")
+for device in gpu_devices:
+    tf.config.experimental.set_memory_growth(device, True)
+
+path="./input/energyconsumptiongenerationpricesandweather/energy_dataset.csv"
+df_energy = pd.read_csv(path)
+
+multivar_df = df_energy[['time','total load actual', 'price actual']]
+multivar_df.head()
+
+df = df_energy[["total load forecast","total load actual"]]
+df_filled = df.interpolate("linear")
+mm = MinMaxScaler()
+df_scaled = mm.fit_transform(df_filled)
+
+df_prep = pd.DataFrame(df_scaled, columns=df.columns)
+y_true = df_prep["total load actual"]
+y_pred_forecast = df_prep["total load forecast"]
+
+y_preds_persistance_1_day = y_true.shift(24).dropna()
+persistence_1_day_mae = tf.keras.losses.MAE(y_true[y_preds_persistance_1_day.index], y_preds_persistance_1_day).numpy()
+persistence_1_day_mape = tf.keras.losses.MAPE(np.maximum(y_true[y_preds_persistance_1_day.index], 1e-5), np.maximum(y_preds_persistance_1_day, 1e-5)).numpy()
+
+ref_error = pd.DataFrame({
+    "Method": ["TSO Forecast", "Persistence 1 Day"],
+    "MAE": [tf.keras.losses.MAE(y_true, y_pred_forecast).numpy(),
+            persistence_1_day_mae],
+    "MAPE":[tf.keras.losses.MAPE(np.maximum(y_true, 1e-5), np.maximum(y_pred_forecast, 1e-5)).numpy(),
+            persistence_1_day_mape]}, 
+    index=[i for i in range(2)])
+
+print("\nSummary of Baseline Errors")
+print(ref_error)
+print(f"\nAverage error in MW for TSO Forecast {round(df['total load forecast'].mean()*ref_error.iloc[0,1], 2)}")
+
+def clean_data(series):
+    """Fills missing values. 
+    
+        Interpolate missing values with a linear approximation.
+    """
+    series_filled = series.interpolate(method='linear')
+        
+    return series_filled
+        
+    
+def min_max_scale(dataframe):
+    """ Applies MinMax Scaling
+    
+        Wrapper for sklearn's MinMaxScaler class.
+    """
+    mm = MinMaxScaler()
+    return mm.fit_transform(dataframe)
+def make_time_features(series):
+    
+    #convert series to datetimes
+    times = series.apply(lambda x: x.split('+')[0])
+    datetimes = pd.DatetimeIndex(times)
+    
+    hours = datetimes.hour.values
+    day = datetimes.dayofweek.values
+    months = datetimes.month.values
+    
+    hour = pd.Series(hours, name='hours')
+    dayofw = pd.Series(day, name='dayofw')
+    month = pd.Series(months, name='months')
+    
+    return hour, dayofw, month
+
+hour, day, month = make_time_features(multivar_df.time)
+print("Hours")
+print(hour.head())
+print("Day of Week")
+print(day.head())
+print("Months")
+print(month.head())
+
+def split_data(series, train_fraq, test_len=8760):
+    """Splits input series into train, val and test.
+    
+        Default to 1 year of test data.
+    """
+    #slice the last year of data for testing 1 year has 8760 hours
+    test_slice = len(series)-test_len
+
+    test_data = series[test_slice:]
+    train_val_data = series[:test_slice]
+
+    #make train and validation from the remaining
+    train_size = int(len(train_val_data) * train_fraq)
+    
+    train_data = train_val_data[:train_size]
+    val_data = train_val_data[train_size:]
+    
+    return train_data, val_data, test_data
+
+
+multivar_df = clean_data(multivar_df)
+
+#add hour and month features
+hours, day, months = make_time_features(multivar_df.time)
+multivar_df = pd.concat([multivar_df.drop(['time'], axis=1), hours, day, months], axis=1)
+
+#scale
+multivar_df = min_max_scale(multivar_df)
+train_multi, val_multi, test_multi = split_data(multivar_df, train_fraq=0.65, test_len=8760)
+print("Multivarate Datasets")
+print(f"Train Data Shape: {train_multi.shape}")
+print(f"Val Data Shape: {val_multi.shape}")
+print(f"Test Data Shape: {test_multi.shape}")
+print(f"Nulls In Train {np.any(np.isnan(train_multi))}")
+print(f"Nulls In Validation {np.any(np.isnan(val_multi))}")
+print(f"Nulls In Test {np.any(np.isnan(test_multi))}")
+def window_dataset(data, n_steps, n_horizon, batch_size, shuffle_buffer, multi_var=False, expand_dims=False):
+    """ Create a windowed tensorflow dataset
+    
+    """
+
+    #create a window with n steps back plus the size of the prediction length
+    window = n_steps + n_horizon
+    
+    #expand dimensions to 3D to fit with LSTM inputs
+    #creat the inital tensor dataset
+    if expand_dims:
+        ds = tf.expand_dims(data, axis=-1)
+        ds = tf.data.Dataset.from_tensor_slices(ds)
+    else:
+        ds = tf.data.Dataset.from_tensor_slices(data)
+    
+    #create the window function shifting the data by the prediction length
+    ds = ds.window(window, shift=n_horizon, drop_remainder=True)
+    
+    #flatten the dataset and batch into the window size
+    ds = ds.flat_map(lambda x : x.batch(window))
+    ds = ds.shuffle(shuffle_buffer)    
+    
+    #create the supervised learning problem x and y and batch
+    if multi_var:
+        ds = ds.map(lambda x : (x[:-n_horizon], x[-n_horizon:, :1]))
+    else:
+        ds = ds.map(lambda x : (x[:-n_horizon], x[-n_horizon:]))
+    
+    ds = ds.batch(batch_size).prefetch(1)
+    
+    return ds
+
+tf.random.set_seed(42)
+
+n_steps = 72
+n_horizon = 24
+batch_size = 1
+shuffle_buffer = 100
+
+
+ds = window_dataset(train_multi, n_steps, n_horizon, batch_size, shuffle_buffer, multi_var=True)
+
+print('Example sample shapes')
+for idx,(x,y) in enumerate(ds):
+    print("x = ", x.numpy().shape)
+    print("y = ", y.numpy().shape)
+    break
+    
+def build_dataset(train_fraq=0.65, 
+                  n_steps=24*30, 
+                  n_horizon=24, 
+                  batch_size=256, 
+                  shuffle_buffer=500, 
+                  expand_dims=False, 
+                  multi_var=False):
+    """If multi variate then first column is always the column from which the target is contstructed.
+    """
+    
+    tf.random.set_seed(23)
+    
+    if multi_var:
+        path="./input/energyconsumptiongenerationpricesandweather/energy_dataset.csv"
+        df_mv = pd.read_csv(path)
+
+        data  = df_mv[['time','total load actual', 'price actual']]
+        hours, day, months = make_time_features(data.time)
+        data = pd.concat([data.drop(['time'], axis=1), hours, day, months], axis=1)
+    else:
+        path="./input/energyconsumptiongenerationpricesandweather/energy_dataset.csv"
+        df_mv = pd.read_csv(path)
+
+        data  = df_mv[['total load actual']]
+        
+    data = clean_data(data)
+    
+    if multi_var:
+        mm = MinMaxScaler()
+        data = mm.fit_transform(data)
+    
+    train_data, val_data, test_data = split_data(data, train_fraq=train_fraq, test_len=8760)
+    
+    train_ds = window_dataset(train_data, n_steps, n_horizon, batch_size, shuffle_buffer, multi_var=multi_var, expand_dims=expand_dims)
+    val_ds = window_dataset(val_data, n_steps, n_horizon, batch_size, shuffle_buffer, multi_var=multi_var, expand_dims=expand_dims)
+    test_ds = window_dataset(test_data, n_steps, n_horizon, batch_size, shuffle_buffer, multi_var=multi_var, expand_dims=expand_dims)
+    
+    
+    print(f"Prediction lookback (n_steps): {n_steps}")
+    print(f"Prediction horizon (n_horizon): {n_horizon}")
+    print(f"Batch Size: {batch_size}")
+    print("Datasets:")
+    print(train_ds.element_spec)
+    
+    return train_ds, val_ds, test_ds
+
+train_ds, val_ds, test_ds = build_dataset(multi_var=True)
+def get_params(multivar=False):
+    lr = 3e-4
+    n_steps=24*30
+    n_horizon=24
+    if multivar:
+        n_features=5
+    else:
+        n_features=1
+        
+    return n_steps, n_horizon, n_features, lr
+
+model_configs = dict()
+
+def cfg_model_run(model, history, test_ds):
+    return {"model": model, "history" : history, "test_ds": test_ds}
+
+
+def run_model(model_name, model_func, model_configs, epochs):
+    
+    n_steps, n_horizon, n_features, lr = get_params(multivar=True)
+    train_ds, val_ds, test_ds = build_dataset(n_steps=n_steps, n_horizon=n_horizon, multi_var=True)
+
+    model = model_func(n_steps, n_horizon, n_features, lr=lr)
+
+    model_hist = model.fit(train_ds, validation_data=val_ds, epochs=epochs)
+
+    model_configs[model_name] = cfg_model_run(model, model_hist, test_ds)
+    return test_ds
+def dnn_model(n_steps, n_horizon, n_features, lr):
+    tf.keras.backend.clear_session()
+    
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(n_steps, n_features)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Dense(n_horizon)
+    ], name='dnn')
+    
+    loss=tf.keras.losses.Huber()
+    optimizer = tf.keras.optimizers.Adam(lr=lr)
+    
+    model.compile(loss=loss, optimizer='adam', metrics=['mae', tf.keras.metrics.RootMeanSquaredError()])
+    
+    return model
+
+
+dnn = dnn_model(*get_params(multivar=True))
+dnn.summary()
+def cnn_model(n_steps, n_horizon, n_features, lr=3e-4):
+    
+    tf.keras.backend.clear_session()
+    
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv1D(48, kernel_size=2, strides = 1, activation='relu', input_shape=(n_steps,n_features)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(48, activation='relu',),
+        tf.keras.layers.Dense(n_horizon)
+    ], name="CNN")
+    
+    loss = tf.keras.losses.MeanSquaredError()
+    optimizer = tf.keras.optimizers.Adam(lr=lr)
+    
+    model.compile(loss=loss, optimizer='adam', metrics=['mae',tf.keras.metrics.RootMeanSquaredError()])
+    
+    return model
+
+cnn = cnn_model(*get_params(multivar=True))
+cnn.summary()
+
+def lstm_model(n_steps, n_horizon, n_features, lr):
+    
+    tf.keras.backend.clear_session()
+    
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.LSTM(100, input_shape=(n_steps,n_features), 
+         return_sequences=True),
+
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(200, activation='relu'),
+        tf.keras.layers.Dense(n_horizon)
+    ], name='lstm')
+    
+    loss = tf.keras.losses.MeanSquaredError()
+    optimizer = tf.keras.optimizers.Adam(lr=lr)
+    
+    model.compile(loss=loss, optimizer='adam', metrics=['mae',tf.keras.metrics.RootMeanSquaredError()])
+    
+    return model
+
+lstm = lstm_model(*get_params(multivar=True))
+lstm.summary()
+
+
+def lstm_cnn_model(n_steps, n_horizon, n_features, lr):
+    
+    tf.keras.backend.clear_session()
+    
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv1D(100, kernel_size=2, strides = 1, padding = 'causal', activation='relu', input_shape=(n_steps,n_features)),
+        tf.keras.layers.LSTM(100, activation='relu', return_sequences=True),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(50, activation='relu'),
+        tf.keras.layers.Dense(n_horizon)
+    ], name="lstm_cnn")
+    
+    loss = tf.keras.losses.MeanSquaredError()
+    optimizer = tf.keras.optimizers.Adam(lr=lr)
+    
+    model.compile(loss=loss, optimizer='adam', metrics=['mae',tf.keras.metrics.RootMeanSquaredError()])
+    
+    return model
+
+lstm_cnn = lstm_cnn_model(*get_params(multivar=True))
+lstm_cnn.summary()
+
+model_configs=dict()
+run_model("cnn", cnn_model, model_configs, epochs=150)
+run_model("lstm", lstm_model, model_configs, epochs=150)
+run_model("lstm_cnn", lstm_cnn_model, model_configs, epochs=150)
+run_model("dnn", cnn_model, model_configs, epochs=150)
+legend = list()
+
+fig, axs = plt.subplots(1, 4, figsize=(25,5))
+
+def plot_graphs(metric, val, ax, upper):
+    ax.plot(val['history'].history[metric])
+    ax.plot(val['history'].history[f'val_{metric}'])
+    ax.set_title(key)
+    ax.legend([metric, f"val_{metric}"])
+    ax.set_xlabel('epochs')
+    ax.set_ylabel(metric)
+    ax.set_ylim([0, upper])
+    
+for (key, val), ax in zip(model_configs.items(), axs.flatten()):
+    plot_graphs('loss', val, ax, 0.2)
+print("Loss Curves")
+plt.show()
+print("MAE Curves")
+fig, axs = plt.subplots(1, 4, figsize=(25,5))
+for (key, val), ax in zip(model_configs.items(), axs.flatten()):
+    plot_graphs('mae', val, ax, 0.6)
+
+print("RSME Curves")
+fig, axs = plt.subplots(1, 4, figsize=(25,5))
+for (key, val), ax in zip(model_configs.items(), axs.flatten()):
+    plot_graphs('root_mean_squared_error', val, ax, 0.6)
+plt.show()
+
+names = list()
+performance = list()
+
+for key, value in model_configs.items():
+    names.append(key)
+    mae = value['model'].evaluate(value['test_ds'])
+    performance.append([mae[1],mae[2]])
+    
+performance_df = pd.DataFrame(performance, index=names, columns=['mae','rsme'])
+performance_df['error_mw'] = performance_df['mae'] * df['total load forecast'].mean()
+print(performance_df)    
+
+fig, axs = plt.subplots(4, 1, figsize=(18, 10))
+days = 14
+
+vline = np.linspace(0, days*24, days+1)
+
+for (key, val), ax in zip(model_configs.items(), axs):
+
+    test = val['test_ds']
+    preds = val['model'].predict(test)
+
+    xbatch, ybatch = iter(test).get_next()
+
+    ax.plot(ybatch.numpy()[:days].reshape(-1))
+    ax.plot(preds[:days].reshape(-1))
+    ax.set_title(key)
+    ax.vlines(vline, ymin=0, ymax=1, linestyle='dotted', transform = ax.get_xaxis_transform())
+    ax.legend(["Actual", "Predicted"])
+
+
+plt.xlabel("Hours Cumulative")
+print('First Two Weeks of Predictions')
+plt.show()
