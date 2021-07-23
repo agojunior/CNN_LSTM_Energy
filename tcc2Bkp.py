78dcc66f0918e877087d5e1f1c86b126e0865d3f
Junior <antonio.gomes.o.jr@gmail.com>
2021-07-23 11:31:43 -0300

    clean code

--- tcc2Bkp.py
+++ tcc2Bkp.py
@@ -2,18 +2,19 @@
 # It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np 
+import pandas as pd 
 
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
 import os
 
+gpu_devices = tf.config.experimental.list_physical_devices("GPU")
+for device in gpu_devices:
+    tf.config.experimental.set_memory_growth(device, True)
 
 path="./input/energyconsumptiongenerationpricesandweather/energy_dataset.csv"
 df_energy = pd.read_csv(path)
@@ -30,49 +31,21 @@ df_prep = pd.DataFrame(df_scaled, columns=df.columns)
 y_true = df_prep["total load actual"]
 y_pred_forecast = df_prep["total load forecast"]
 
-y_preds_persistance_1_day = y_true.shift(24).dropna()
-persistence_1_day_mae = tf.keras.losses.MAE(y_true[y_preds_persistance_1_day.index], y_preds_persistance_1_day).numpy()
-persistence_1_day_mape = tf.keras.losses.MAPE(np.maximum(y_true[y_preds_persistance_1_day.index], 1e-5), np.maximum(y_preds_persistance_1_day, 1e-5)).numpy()
-
-ref_error = pd.DataFrame({
-    "Method": ["TSO Forecast", "Persistence 1 Day"],
-    "MAE": [tf.keras.losses.MAE(y_true, y_pred_forecast).numpy(),
-            persistence_1_day_mae],
-    "MAPE":[tf.keras.losses.MAPE(np.maximum(y_true, 1e-5), np.maximum(y_pred_forecast, 1e-5)).numpy(),
-            persistence_1_day_mape]}, 
-    index=[i for i in range(2)])
-
-print("\nSummary of Baseline Errors")
-print(ref_error)
-print(f"\nAverage error in MW for TSO Forecast {round(df['total load forecast'].mean()*ref_error.iloc[0,1], 2)}")
-
 def clean_data(series):
-    """Fills missing values. 
-    
-        Interpolate missing values with a linear approximation.
-    """
     series_filled = series.interpolate(method='linear')
-        
     return series_filled
         
     
 def min_max_scale(dataframe):
-    """ Applies MinMax Scaling
-    
-        Wrapper for sklearn's MinMaxScaler class.
-    """
     mm = MinMaxScaler()
     return mm.fit_transform(dataframe)
+
 def make_time_features(series):
-    
-    #convert series to datetimes
     times = series.apply(lambda x: x.split('+')[0])
     datetimes = pd.DatetimeIndex(times)
-    
     hours = datetimes.hour.values
     day = datetimes.dayofweek.values
     months = datetimes.month.values
-    
     hour = pd.Series(hours, name='hours')
     dayofw = pd.Series(day, name='dayofw')
     month = pd.Series(months, name='months')
@@ -80,27 +53,12 @@ def make_time_features(series):
     return hour, dayofw, month
 
 hour, day, month = make_time_features(multivar_df.time)
-print("Hours")
-print(hour.head())
-print("Day of Week")
-print(day.head())
-print("Months")
-print(month.head())
 
 def split_data(series, train_fraq, test_len=8760):
-    """Splits input series into train, val and test.
-    
-        Default to 1 year of test data.
-    """
-    #slice the last year of data for testing 1 year has 8760 hours
     test_slice = len(series)-test_len
-
     test_data = series[test_slice:]
     train_val_data = series[:test_slice]
-
-    #make train and validation from the remaining
     train_size = int(len(train_val_data) * train_fraq)
-    
     train_data = train_val_data[:train_size]
     val_data = train_val_data[train_size:]
     
@@ -109,49 +67,29 @@ def split_data(series, train_fraq, test_len=8760):
 
 multivar_df = clean_data(multivar_df)
 
-#add hour and month features
 hours, day, months = make_time_features(multivar_df.time)
 multivar_df = pd.concat([multivar_df.drop(['time'], axis=1), hours, day, months], axis=1)
 
-#scale
 multivar_df = min_max_scale(multivar_df)
 train_multi, val_multi, test_multi = split_data(multivar_df, train_fraq=0.65, test_len=8760)
-print("Multivarate Datasets")
-print(f"Train Data Shape: {train_multi.shape}")
-print(f"Val Data Shape: {val_multi.shape}")
-print(f"Test Data Shape: {test_multi.shape}")
-print(f"Nulls In Train {np.any(np.isnan(train_multi))}")
-print(f"Nulls In Validation {np.any(np.isnan(val_multi))}")
-print(f"Nulls In Test {np.any(np.isnan(test_multi))}")
-def window_dataset(data, n_steps, n_horizon, batch_size, shuffle_buffer, multi_var=False, expand_dims=False):
-    """ Create a windowed tensorflow dataset
-    
-    """
 
-    #create a window with n steps back plus the size of the prediction length
+def window_dataset(data, n_steps, n_horizon, batch_size, shuffle_buffer, expand_dims=False):
+
     window = n_steps + n_horizon
-    
-    #expand dimensions to 3D to fit with LSTM inputs
-    #creat the inital tensor dataset
     if expand_dims:
         ds = tf.expand_dims(data, axis=-1)
         ds = tf.data.Dataset.from_tensor_slices(ds)
     else:
         ds = tf.data.Dataset.from_tensor_slices(data)
     
-    #create the window function shifting the data by the prediction length
     ds = ds.window(window, shift=n_horizon, drop_remainder=True)
     
-    #flatten the dataset and batch into the window size
     ds = ds.flat_map(lambda x : x.batch(window))
     ds = ds.shuffle(shuffle_buffer)    
     
-    #create the supervised learning problem x and y and batch
-    if multi_var:
-        ds = ds.map(lambda x : (x[:-n_horizon], x[-n_horizon:, :1]))
-    else:
-        ds = ds.map(lambda x : (x[:-n_horizon], x[-n_horizon:]))
-    
+
+    ds = ds.map(lambda x : (x[:-n_horizon], x[-n_horizon:, :1]))
+
     ds = ds.batch(batch_size).prefetch(1)
     
     return ds
@@ -164,70 +102,47 @@ batch_size = 1
 shuffle_buffer = 100
 
 
-ds = window_dataset(train_multi, n_steps, n_horizon, batch_size, shuffle_buffer, multi_var=True)
-
-print('Example sample shapes')
-for idx,(x,y) in enumerate(ds):
-    print("x = ", x.numpy().shape)
-    print("y = ", y.numpy().shape)
-    break
+ds = window_dataset(train_multi, n_steps, n_horizon, batch_size, shuffle_buffer)
     
 def build_dataset(train_fraq=0.65, 
                   n_steps=24*30, 
                   n_horizon=24, 
                   batch_size=256, 
                   shuffle_buffer=500, 
-                  expand_dims=False, 
-                  multi_var=False):
-    """If multi variate then first column is always the column from which the target is contstructed.
-    """
+                  expand_dims=False):
+
     
     tf.random.set_seed(23)
     
-    if multi_var:
-        path="./input/energyconsumptiongenerationpricesandweather/energy_dataset.csv"
-        df_mv = pd.read_csv(path)
+    path="./input/energyconsumptiongenerationpricesandweather/energy_dataset.csv"
+    df_mv = pd.read_csv(path)
 
-        data  = df_mv[['time','total load actual', 'price actual']]
-        hours, day, months = make_time_features(data.time)
-        data = pd.concat([data.drop(['time'], axis=1), hours, day, months], axis=1)
-    else:
-        path="./input/energyconsumptiongenerationpricesandweather/energy_dataset.csv"
-        df_mv = pd.read_csv(path)
-
-        data  = df_mv[['total load actual']]
+    data  = df_mv[['time','total load actual', 'price actual']]
+    hours, day, months = make_time_features(data.time)
+    data = pd.concat([data.drop(['time'], axis=1), hours, day, months], axis=1)
         
     data = clean_data(data)
     
-    if multi_var:
-        mm = MinMaxScaler()
-        data = mm.fit_transform(data)
+
+    mm = MinMaxScaler()
+    data = mm.fit_transform(data)
     
     train_data, val_data, test_data = split_data(data, train_fraq=train_fraq, test_len=8760)
     
-    train_ds = window_dataset(train_data, n_steps, n_horizon, batch_size, shuffle_buffer, multi_var=multi_var, expand_dims=expand_dims)
-    val_ds = window_dataset(val_data, n_steps, n_horizon, batch_size, shuffle_buffer, multi_var=multi_var, expand_dims=expand_dims)
-    test_ds = window_dataset(test_data, n_steps, n_horizon, batch_size, shuffle_buffer, multi_var=multi_var, expand_dims=expand_dims)
-    
-    
-    print(f"Prediction lookback (n_steps): {n_steps}")
-    print(f"Prediction horizon (n_horizon): {n_horizon}")
-    print(f"Batch Size: {batch_size}")
-    print("Datasets:")
-    print(train_ds.element_spec)
+    train_ds = window_dataset(train_data, n_steps, n_horizon, batch_size, shuffle_buffer , expand_dims=expand_dims)
+    val_ds = window_dataset(val_data, n_steps, n_horizon, batch_size, shuffle_buffer, expand_dims=expand_dims)
+    test_ds = window_dataset(test_data, n_steps, n_horizon, batch_size, shuffle_buffer, expand_dims=expand_dims)
+
     
     return train_ds, val_ds, test_ds
 
-train_ds, val_ds, test_ds = build_dataset(multi_var=True)
-def get_params(multivar=False):
+train_ds, val_ds, test_ds = build_dataset()
+def get_params():
     lr = 3e-4
     n_steps=24*30
     n_horizon=24
-    if multivar:
-        n_features=5
-    else:
-        n_features=1
-        
+    n_features=5
+
     return n_steps, n_horizon, n_features, lr
 
 model_configs = dict()
@@ -238,8 +153,8 @@ def cfg_model_run(model, history, test_ds):
 
 def run_model(model_name, model_func, model_configs, epochs):
     
-    n_steps, n_horizon, n_features, lr = get_params(multivar=True)
-    train_ds, val_ds, test_ds = build_dataset(n_steps=n_steps, n_horizon=n_horizon, multi_var=True)
+    n_steps, n_horizon, n_features, lr = get_params()
+    train_ds, val_ds, test_ds = build_dataset(n_steps=n_steps, n_horizon=n_horizon)
 
     model = model_func(n_steps, n_horizon, n_features, lr=lr)
 
@@ -247,7 +162,7 @@ def run_model(model_name, model_func, model_configs, epochs):
 
     model_configs[model_name] = cfg_model_run(model, model_hist, test_ds)
     return test_ds
-
+    
 def cnn_model(n_steps, n_horizon, n_features, lr=3e-4):
     
     tf.keras.backend.clear_session()
